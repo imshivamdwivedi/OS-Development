bb8308d548941bc9f9b70403eb4f17acdb7cf0ae
imshivamdwivedi <dwivedishivam758@gmail.com>
2019-10-12 23:44:26 +0530

    Added Interrupt descriptor table

---
+++ include/util.h
@@ -0,0 +1,11 @@
+#ifdef UTIL_H
+#define UTIL_H
+
+#include "types.h"
+
+void memory_copy(char *source, char *dest, int nbytes);
+void memory_set(char *dest, uint8 val, uint32 len);
+void int_to_ascii(int n , char str[]);
+
+#endif
+
