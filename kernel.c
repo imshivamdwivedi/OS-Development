67ade426aa2354a9d1da725b685396f892955937
imshivamdwivedi <dwivedishivam758@gmail.com>
2019-10-13 00:22:54 +0530

    Added Interrupt Service Routine

--- kernel.c
+++ kernel.c
@@ -1,6 +1,7 @@
 #include "include/kb.h"
 int kmain()
 {
+       isr_install();
        clearScreen();
        print("Hi and Welcome to my operating system\nPlease enter a command");
        printch('\n');
