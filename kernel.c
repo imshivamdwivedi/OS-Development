d8a0f0f3a09cf64eb69c9a78c05abbd3cf6b142a
vikramc <siddhartha779@gmail.com>
2019-11-11 12:02:49 +0530

    minor changes

--- kernel.c
+++ kernel.c
@@ -5,25 +5,25 @@ int kmain()
        print("Hi and Welcome to my operating system\nPlease enter a command");
        printch('\n');
        print("OS> ");
-       while (1)
+       while(1)
        {
                 string ch = readStr();
                 print("\n");
-                print(ch);
-               /* if(strEql(ch,"cmd"))
+                //print(ch);
+                if(strEql(ch,"cmd"))
                 {
-                        print("\nYou are allready in cmd\n");
+                        print("\nYou are already in cmd\n");
                 }
                 else if(strEql(ch,"clear"))
                 {
                         clearScreen();
                         print("OS> ");
                 }
-                
                 else
                 {
-                        print("Bad command!");
-                } */       
+                        print("\nBad command!\n");
+                }    
+		print("OS> ");
        }
         
 }
